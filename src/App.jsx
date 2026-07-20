b01f31e4793c3d5c9a9b67a5a665e3b876f6474a
Aryan Kulkarni <kulkarniaryan852@gmail.com>
2026-07-20 19:57:28 +0530

    routing

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./routing/navbar";
+import home from "./routing/Home";
+import service from "./routing/Services";
+import contact from "./routing/Contact";
+import About from "./routing/About";
+
+function App(){
+  return(
+    <>
+    <Navbar/>
+    </>
+  );
+}
+export default App;
