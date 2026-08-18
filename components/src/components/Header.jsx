29f004a71b6916a7c10401d9484edc98b44cd7d2
Aryan Kulkarni <kulkarniaryan852@gmail.com>
2026-08-18 21:53:22 +0530

    Components

---
+++ components/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <>
+        <h1>Login form</h1>
+        <h2>Welcome to my website</h2>
+        </>
+    );
+}
+export default Header;
---
+++ components/src/components/Login.jsx
@@ -0,0 +1,13 @@
+function Login(){
+    return(
+    <>
+    <div className="container">
+    <h1 className="heading">Login</h1>
+    <input type="text" placeholder="Name"/>
+    <input type="email" placeholder="Email"/>
+    <input type="password" placeholder="Enter Password"/>
+    </div>
+    </>
+    );
+}
+export default Login;
