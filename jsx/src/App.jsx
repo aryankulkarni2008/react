279efa5d20ab39a97041939280038e1294bf1586
Aryan Kulkarni <kulkarniaryan852@gmail.com>
2026-08-18 21:51:39 +0530

    Simple

---
+++ jsx/src/App.jsx
@@ -0,0 +1,12 @@
+function App(){
+    const name = "Aryan Kulkarni";
+    return(
+        <>
+  <h1>Student Profile</h1>
+  <h2>Aryan Kulkarni</h2>
+<h2>Name{name}</h2>
+  <h2>19</h2>
+  </>
+    );
+}
+export default App;
